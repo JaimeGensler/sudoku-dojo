883cbf88b315291dfb5d3a8fd2d92b6d79cd5c5a
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-21 13:01:13 -0700

    Cleanup

--- components/Github.tsx
+++ components/Github.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from 'antd';
 import styled from 'styled-components';
 import { GithubOutlined } from '@ant-design/icons';
 
-const Corner = styled.div`
+const A = styled.a`
     position: absolute;
     left: 1rem;
     bottom: 1rem;
@@ -10,31 +10,23 @@ const Corner = styled.div`
     padding: 0.25rem;
     border: 1px solid #bebebe;
     border-radius: 50%;
-
-    &:hover {
+    font-size: 2rem;
+    line-height: 2rem;
+    color: black;
+    &:hover,
+    &:focus {
         background-color: black;
         color: white;
         border: 0px;
     }
 `;
 
-const A = styled.a`
-    font-size: 2rem;
-    line-height: 2rem;
-    color: black;
-    &:hover {
-        color: white;
-    }
-`;
-
 export default function Github() {
     return (
-        <Corner>
-            <A href="https://www.github.com/JaimeGensler/sudoku-dojo">
-                <Tooltip placement="right" title="See source code on Github">
-                    <GithubOutlined />
-                </Tooltip>
-            </A>
-        </Corner>
+        <A href="https://www.github.com/JaimeGensler/sudoku-dojo">
+            <Tooltip placement="right" title="See source code on Github">
+                <GithubOutlined />
+            </Tooltip>
+        </A>
     );
 }
