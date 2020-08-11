a5c394e6e0437845250d664ecf4e539a5e6a13f2
Jaime Gensler <jaimegensler0@gmail.com>
2020-08-11 08:18:30 -0700

    Dist commit

--- src/components/utils/useKeydown.ts
+++
@@ -1,6 +0,0 @@
-import { onBeforeMount, onBeforeUnmount } from 'vue';
-
-export default function useKeydown(callback: (e: KeyboardEvent) => void) {
-    onBeforeMount(() => window.addEventListener('keydown', callback));
-    onBeforeUnmount(() => window.removeEventListener('keydown', callback));
-}
--- src/lib/sudoku/index.ts
+++
@@ -1,9 +0,0 @@
-import initialize from './initialize';
-import rules from './rules';
-import keyMap from './keyMap';
-
-export default {
-    initialize,
-    rules,
-    keyMap,
-};
