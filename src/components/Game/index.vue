f7be1d6c2398af743bfef0591dd444786b8441fe
Jaime Gensler <jaimegensler0@gmail.com>
2020-08-12 11:10:12 -0700

    Hack around Tailwind purging

--- src/components/Game/index.vue
+++ src/components/Game/index.vue
@@ -8,6 +8,31 @@
 import { defineComponent } from 'vue';
 import Block from './Block.vue';
 
+// This is kind of silly but Tailwind's purge options weren't cooperating so here we are
+const twcssDoNotPurgeList = [
+    'bg-blue-200',
+    'text-blue-700',
+    'bg-purple-200',
+    'text-purple-700',
+    'bg-red-200',
+    'text-red-700',
+    'bg-blue-700',
+    'text-blue-100',
+    'bg-purple-700',
+    'text-purple-100',
+    'bg-red-700',
+    'text-red-100',
+
+    'border-l-0',
+    'border-r-0',
+    'border-blue-800',
+    'text-blue-800',
+    'bg-blue-200',
+    'border-l',
+    'border-r',
+    'border-t',
+    'border-b',
+];
 export default defineComponent({
     components: { Block },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,5 @@
 module.exports = {
-    purge: {
-        enabled: process.env.NODE_ENV === 'production',
-        content: [
-            './src/components/**/*.vue',
-            './src/components/Game/Cell/helpers.ts',
-        ],
-        options: {
-            whitelist: [
-                'bg-blue-200',
-                'text-blue-700',
-                'bg-purple-200',
-                'text-purple-700',
-                'bg-red-200',
-                'text-red-700',
-                'bg-blue-700',
-                'text-blue-100',
-                'bg-purple-700',
-                'text-purple-100',
-                'bg-red-700',
-                'text-red-100',
-
-                'border-l-0',
-                'border-r-0',
-                'border-blue-800',
-                'text-blue-800',
-                'bg-blue-200',
-                'border-l',
-                'border-r',
-                'border-t',
-                'border-b',
-            ],
-        },
-    },
+    purge: [],
     theme: {
         extend: {
             spacing: {
